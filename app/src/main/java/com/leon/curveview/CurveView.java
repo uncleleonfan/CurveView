c53390db233de1d8b73abc9aee6b3917ac917231
Leon <526247082@qq.com>
2017-06-13 21:11:16 +0800

    Add comment.

--- app/src/main/java/com/leon/curveview/CurveView.java
+++ app/src/main/java/com/leon/curveview/CurveView.java
@@ -8,7 +8,6 @@ import android.graphics.Path;
 import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 
@@ -21,29 +20,23 @@ import java.util.List;
 
 public class CurveView extends View {
 
-    private static final String TAG = "CurveView";
-
     private final Paint mAxisPaint;
     private final int mGap;
     private final int mRadius;
     private Path mCurvePath;
     private Paint mPaint;
-    private Paint mControlPaint;
     private int[] mDataList;
     private int mMax;
     private String[] mHorizontalAxis;
 
     private int mNormalDotColor = Color.BLACK;
 
-
     private List<Dot> mDots = new ArrayList<Dot>();
-    private List<Dot> mControlDotsList = new ArrayList<Dot>();
-
     private Rect mTextRect;
     private int mStep;
     private Paint mDotPaint;
 
-    private float smoothnessRatio = 0.16f;
+    private static final float SMOOTHNESS_RATIO = 0.16f;
 
     private float[][] mControlDots = new float[2][2];
 
@@ -61,10 +54,6 @@ public class CurveView extends View {
         mPaint.setStrokeWidth(3);
         mPaint.setColor(Color.BLUE);
 
-        mControlPaint = new Paint();
-        mControlPaint.setAntiAlias(true);
-        mControlPaint.setColor(Color.GREEN);
-
         mAxisPaint = new Paint();
         mAxisPaint.setAntiAlias(true);
         mAxisPaint.setTextSize(20);
@@ -76,7 +65,6 @@ public class CurveView extends View {
 
         mGap = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, getResources().getDisplayMetrics());
         mTextRect = new Rect();
-
         mCurvePath = new Path();
     }
 
@@ -87,35 +75,32 @@ public class CurveView extends View {
         int height = h - getPaddingTop() - getPaddingBottom();
         mStep = width / (mDataList.length - 1);
 
-
         mAxisPaint.getTextBounds(mHorizontalAxis[0], 0, mHorizontalAxis[0].length(), mTextRect);
-        int barHeight = height - mTextRect.height() - mGap;
-        float heightRatio = barHeight / mMax;
+        int maxBarHeight = height - mTextRect.height() - mGap;
+        float heightRatio = maxBarHeight / mMax;
 
         for (int i = 0; i < mDataList.length; i++) {
-
             Dot dot = new Dot();
             dot.value = mDataList[i];
             dot.transformedValue = dot.value * heightRatio;
 
             dot.x = mStep * i + getPaddingLeft();
-            dot.y = getPaddingTop() + barHeight - dot.transformedValue;
+            dot.y = getPaddingTop() + maxBarHeight - dot.transformedValue;
 
             mDots.add(dot);
         }
-
+        //规划曲线路径
         for (int i = 0; i < mDataList.length - 1; i++) {
-
+            //如果是第一个点，就将路径移动到该点
             if (i == 0) {
                 mCurvePath.moveTo(mDots.get(0).x, mDots.get(0).y);
             }
-
+            //计算三阶贝塞尔曲线的控制点
             calculateControlPoints(i);
-
+            //使用三阶贝塞尔曲线连接下一个点
             mCurvePath.cubicTo(mControlDots[0][0], mControlDots[0][1],
                     mControlDots[1][0], mControlDots[1][1],
                     mDots.get(i + 1).x, mDots.get(i + 1).y);
-
         }
 
     }
@@ -123,6 +108,7 @@ public class CurveView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawPath(mCurvePath, mPaint);
+        //绘制点和底部坐标文本
         for (int i = 0; i < mDots.size(); i++) {
             String axis = mHorizontalAxis[i];
             int x = getPaddingLeft() + i * mStep;
@@ -131,12 +117,6 @@ public class CurveView extends View {
             Dot dot = mDots.get(i);
             mDotPaint.setColor(mNormalDotColor);
             canvas.drawCircle(dot.x, dot.y, mRadius, mDotPaint);
-        }
-
-        for (int i = 0; i < mControlDotsList.size(); i++) {
-            Dot dot = mControlDotsList.get(i);
-            canvas.drawCircle(dot.x, dot.y, mRadius, mControlPaint);
-            Log.d(TAG, "draw controll: ");
         }
     }
 
@@ -149,73 +129,59 @@ public class CurveView extends View {
         mHorizontalAxis = horizontalAxis;
     }
 
+    /**
+     * 计算三阶贝塞尔曲线的控制点
+     */
     public void calculateControlPoints(int i) {
-        float x1 = Float.NaN;
-        float y1 = Float.NaN;
-        float x2 = Float.NaN;
-        float y2 = Float.NaN;
-
-        Dot currentDot = mDots.get(i);
-        Dot nextDot = null;
-        Dot previousDot = null;
-        Dot nextNextDot = null;
-
+        //第一个控制点
+        float x1, y1;
+        //第二个控制点
+        float x2, y2;
+        Dot currentDot = mDots.get(i);//当前点
+        Dot nextDot;//下一个点
+        Dot previousDot;//上一个点
+        Dot nextNextDot;//下下一个点
 
         if (i > 0) {
+            //当i>0，即不是第一个点时，获取上一个点
             previousDot = mDots.get(i - 1);
         } else {
+            //当i=0，即为第一个点时，没有上一个点，就用第一个点代替
             previousDot = currentDot;
         }
 
-
-        if (i < mDots.size() -1) {
+        if (i < mDots.size()-1) {
+            //当i没有遍历到最后一个点时，获取下一个点
             nextDot = mDots.get(i + 1);
         } else {
+            //当i=mDots.size()-1，即为最后一个点时，没有下一个点，就用最后一个点代替
             nextDot = currentDot;
         }
 
-
         if (i < mDots.size() - 2) {
+            //当i没有遍历到倒数第二个点时，获取下下个点
             nextNextDot = mDots.get(i + 2);
         } else {
+            //当i遍历到倒数第二个点，下下个点不存在，就用下个点代替
             nextNextDot = nextDot;
         }
-
-
-        Log.d(TAG, "calculateControlPoints: " + previousDot.x  + " " + currentDot.x + "  " + nextDot.x + "  " + nextNextDot.x);
-        x1 = currentDot.x + smoothnessRatio * (nextDot.x - previousDot.x);
-        y1 = currentDot.y + smoothnessRatio * (nextDot.y - previousDot.y);
-
-        x2 = nextDot.x - smoothnessRatio * (nextNextDot.x - currentDot.x);
-        y2 = nextDot.y - smoothnessRatio * (nextNextDot.y - currentDot.y);
-
-        Log.d(TAG, "calculateControlPoints: " + x1 + " " + y1 + " " + x2 + " " + y2);
-
+        //利用公式计算两个控制点，参数取a=b=SMOOTHNESS_RATIO=0.16
+        x1 = currentDot.x + SMOOTHNESS_RATIO * (nextDot.x - previousDot.x);
+        y1 = currentDot.y + SMOOTHNESS_RATIO * (nextDot.y - previousDot.y);
+        x2 = nextDot.x - SMOOTHNESS_RATIO * (nextNextDot.x - currentDot.x);
+        y2 = nextDot.y - SMOOTHNESS_RATIO * (nextNextDot.y - currentDot.y);
+        //保存计算结果到数组中，规划mCurvePath中使用
         mControlDots[0][0] = x1;
         mControlDots[0][1] = y1;
         mControlDots[1][0] = x2;
         mControlDots[1][1] = y2;
-
-
-
-        Dot firstControl = new Dot();
-        firstControl.x = x1;
-        firstControl.y = y1;
-
-        Dot secondControl = new Dot();
-        secondControl.x = x2;
-        secondControl.y = y2;
-
-        mControlDotsList.add(firstControl);
-        mControlDotsList.add(secondControl);
     }
 
     private class Dot {
-        float x = Float.NaN;
-        float y = Float.NaN;
+        float x;
+        float y;
         int value;
         float transformedValue;
     }
-
 
 }
